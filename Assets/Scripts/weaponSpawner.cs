3bc591d17086a4902b2322292ed72f74e2bdb82b
Remi BRIEUC <rbrieuc@e2r10p6.42.fr>
2015-06-20 15:41:25 +0200

    add weapon spawner

---
+++ Assets/Scripts/weaponSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class weaponSpawner : MonoBehaviour {
+
+	public GameObject[] weapons;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (weapons [Random.Range (0, weapons.Length)], transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
