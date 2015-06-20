057b2559cbbb63f7967417f04d92c92d08a6eb1d
Remi BRIEUC <rbrieuc@e2r10p6.42.fr>
2015-06-20 11:38:01 +0200

    camera

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour {
+
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		if (target)
+		{
+			Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+		
+	}
+}
