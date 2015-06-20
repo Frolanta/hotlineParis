7e61fadad80551b917296a2c2502918a44effe93
Remi BRIEUC <rbrieuc@e2r10p6.42.fr>
2015-06-20 19:11:22 +0200

    start enemy

---
+++ Assets/Scripts/oneHitDead.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class oneHitDead : MonoBehaviour {
+
+	public AudioSource audioSource;
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void takeDamage() {
+		animator.SetTrigger ("dead");
+		Destroy (this.gameObject, 1);
+		Destroy (this.gameObject.GetComponent<Rigidbody2D>());
+		Destroy (this.gameObject.GetComponent<Collider2D>());
+		audioSource.Play ();
+	}
+}
