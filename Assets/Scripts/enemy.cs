5d549bd8e675675835ce4d230e440c9f6405b2df
Remi BRIEUC <rbrieuc@e2r10p6.42.fr>
2015-06-21 12:14:08 +0200

    door AI

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -23,7 +23,7 @@ public class enemy : MonoBehaviour {
 	private bool hasIntermediate;
 
 
-	private List<Vector3> exists;
+	private List<Vector3> exits;
 
 	// Use this for initialization
 	void Start () {
@@ -37,7 +37,21 @@ public class enemy : MonoBehaviour {
 		}
 
 
-		exists = new List<Vector3>();
+		exits = new List<Vector3>();
+
+
+		GameObject[] doors = GameObject.FindGameObjectsWithTag("door");
+
+		foreach (GameObject door in doors) {
+			exits.Add(door.transform.position);
+		}
+
+
+		GameObject[] exts = GameObject.FindGameObjectsWithTag("exit");
+		
+		foreach (GameObject e in exts) {
+			exits.Add(e.transform.position);
+		}
 	}
 	
 	// Update is called once per frame
@@ -56,12 +70,13 @@ public class enemy : MonoBehaviour {
 			rb.MovePosition (transform.position + getDirection (intermediatePos) * speed * Time.fixedDeltaTime);
 			lookAtPosition (intermediatePos);
 
-			if (Vector3.Distance (transform.position, checkPosition) <= Time.fixedDeltaTime * speed) {
+			if (Vector3.Distance (transform.position, intermediatePos) <= Time.fixedDeltaTime * speed) {
+				Debug.Log ("intermediate finish");
 				hasIntermediate = false;
 			}
 
 
-		} else if (shouldCheck) {
+		} else if (shouldCheck && richable(checkPosition)) {
 
 			rb.MovePosition (transform.position + getDirection (checkPosition) * speed * Time.fixedDeltaTime);
 			lookAtPosition (checkPosition);
@@ -71,7 +86,7 @@ public class enemy : MonoBehaviour {
 			}
 		
 
-		} else if (hasCheckpoints) {
+		} else if (hasCheckpoints && richable(nextCheckpoint)) {
 
 			rb.MovePosition (transform.position + getDirection (nextCheckpoint) * speed * Time.fixedDeltaTime);
 			lookAtPosition (nextCheckpoint);
@@ -87,7 +102,7 @@ public class enemy : MonoBehaviour {
 			}
 		
 
-		} else if (Vector3.Distance (transform.position, nextCheckpoint) >= speed * Time.fixedDeltaTime) {
+		} else if (Vector3.Distance (transform.position, nextCheckpoint) >= speed * Time.fixedDeltaTime && richable(nextCheckpoint)) {
 			rb.MovePosition (transform.position + getDirection (nextCheckpoint) * speed * Time.fixedDeltaTime);
 			lookAtPosition (nextCheckpoint);
 		} else {
@@ -98,17 +113,57 @@ public class enemy : MonoBehaviour {
 
 
 	bool richable(Vector3 pos) {
-		LayerMask layerMask = ~(1 << LayerMask.NameToLayer ("enemy") | 1 << LayerMask.NameToLayer ("weapon"));
+
+		Debug.Log ("richable test");
+		LayerMask layerMask = ~((1 << LayerMask.NameToLayer ("enemy"))
+		                        | (1 << LayerMask.NameToLayer ("weapon"))
+		                        | (1 << LayerMask.NameToLayer ("door"))
+		                        | (1 << LayerMask.NameToLayer ("Player"))
+		                        );
 		
 		float maxRange = Vector3.Distance(transform.position, pos);
 		RaycastHit2D hit = Physics2D.Raycast(transform.position, (pos - transform.position), maxRange, layerMask);
 
+		Debug.DrawLine(transform.position, pos, Color.green, 0.1f, false);
+
 		if (hit.collider != null) {
 
 
+			Debug.Log ("not richable : " + hit.collider.tag);
 
-			return false;
+			Vector3 exit = Vector3.zero;
+			float dist = 10000.0f;
+			bool hasExit = false;
+
+			foreach (Vector3 e in exits) {
+
+				if (Vector3.Distance (e, transform.position) > 0.01f) {
+					float range = Vector3.Distance(transform.position, e);
+					RaycastHit2D h = Physics2D.Raycast(transform.position, (e - transform.position), range, layerMask);
+					
+					if (h.collider == null) {
+						if (Vector3.Distance (e, pos) < dist) {
+							dist = Vector3.Distance (e, transform.position);
+							exit = e;
+							hasExit = true;
+						}
+					}
+				}
+
+			}
+
+			if (hasExit) {
+				hasIntermediate = true;
+				intermediatePos = exit;
+				return false;
+			}
+
+			return true;
+
+		
 		}
+
+		Debug.Log ("richable");
 
 		return true;
 	}
@@ -148,5 +203,15 @@ public class enemy : MonoBehaviour {
 				target = null;
 			}
 		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.tag == "Player") {
+			if (target) {
+				checkPosition = new Vector3 (target.position.x, target.position.y, target.position.z);
+				shouldCheck = true;
+				target = null;
+			}
+		} 
 	}
 }
