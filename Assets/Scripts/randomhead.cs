120c3985b4606880e1d8310e601b0b76681e19ed
Remi BRIEUC <rbrieuc@e2r10p6.42.fr>
2015-06-21 15:48:44 +0200

    random heads

---
+++ Assets/Scripts/randomhead.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomhead : MonoBehaviour {
+
+
+	public Sprite[] heads;
+	public SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		sr.sprite = heads[Random.Range (0, heads.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
